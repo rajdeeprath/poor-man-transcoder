af4239dc4c4c273407067ebbb150e03b193451a7
rajdeeprath <rajdeeprath@gmail.com>
2018-07-01 18:06:47 +0530

    Changed pom structure
    + Updated pom
    + Added main class for testing

---
+++ ffmpeg-transcoder/src/main/java/com/flashvisions/server/rtmp/transcoder/App.java
@@ -0,0 +1,17 @@
+package com.flashvisions.server.rtmp.transcoder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class App {
+	
+	private static Logger logger = LoggerFactory.getLogger(App.class);
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.info("runnning");
+	}
+
+}
